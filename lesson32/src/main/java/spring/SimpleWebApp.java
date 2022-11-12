0e467a901c1127a495cf49ede4fbd77d2f2b44cb
Igor Cijov <cijovigor@yahoo.com>
2022-11-12 20:37:20 +0100

    homework32
    Transfer the functionality implemented in the previous lessons to the Spring Boot platform.

---
+++ lesson32/src/main/java/spring/SimpleWebApp.java
@@ -0,0 +1,22 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Java Pro. Homework #32
+ *
+ * @author Igor Cijov
+ * @version 09 Nov 2022
+ *
+ */
+
+
+@SpringBootApplication
+public class SimpleWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp.class, args);
+	}
+
+}
