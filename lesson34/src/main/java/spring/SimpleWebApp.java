ed96e8df19ebd1fae2767c9635861c2e601b961c
Igor Cijov <cijovigor@yahoo.com>
2022-11-30 15:28:53 +0100

    lesson34
    Spring Boot with H2 Database.
    Testing the H2 Database.

---
+++ lesson34/src/main/java/spring/SimpleWebApp.java
@@ -0,0 +1,23 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Java Pro. Homework #33
+ *
+ * @author Igor Cijov
+ * @version 16 Nov 2022
+ * Spring Boot + Thymeleaf + JPA + Liquibase + Logger
+ *
+ */
+
+
+@SpringBootApplication
+public class SimpleWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp.class, args);
+	}
+
+}
