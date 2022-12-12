bdd9b608af04fcb24a57d18ee2a230dab314d1f1
Igor Cijov <cijovigor@yahoo.com>
2022-12-12 18:22:56 +0100

    lesson36
    Implementation Using OpenAPI Generator

---
+++ lesson36/src/main/java/spring/SimpleWebApp.java
@@ -0,0 +1,21 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Java Pro. Homework #36
+ *
+ * @author Igor Cijov
+ * @version 07 Dec 2022
+ * Implementation Using OpenAPI Generator
+ *
+ */
+
+@SpringBootApplication
+public class SimpleWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp.class, args);
+	}
+}
